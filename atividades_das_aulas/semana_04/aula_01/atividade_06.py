be236da5c9a9d54972db57245ca12c5be4cd429b
Mugrax <Mugraxdasilva@gmail.com>
2022-06-09 07:16:50 -0300

    finalizando_atividade_06_mas_ainda_com_muitos_problemas

--- atividades_das_aulas/semana_04/aula_01/atividade_06.py
+++ atividades_das_aulas/semana_04/aula_01/atividade_06.py
@@ -3,24 +3,26 @@ lista_homen_acima=[]
 lista_mulher_acima=[]
 lista_homen_abaixo=[]
 lista_mulher_abaixo=[]
-DA={
-    'mulher_acima_da_media':lista_mulher_acima,
-    'mulher_abaixo_da_media':lista_mulher_abaixo,
-    'homen_acima_da_media':lista_homen_acima,
-    'homen_abaixo_da_media':lista_homen_abaixo}
+lista_nomes=[]
 while altura>=0:
     altura=float(input('digite a altura:\n'))
     sexo=input('digite homen ou mulher:\n')
+    if altura>=0:
+        lista_nomes.append(sexo)
     if sexo=='mulher':
-        if altura>1.60:
+        if altura>=1.60:
             lista_mulher_acima.append(altura)
         elif altura<1.60:
             lista_mulher_abaixo.append(altura)
     elif sexo=='homen':
-        if altura>1.73:
+        if altura>=1.73:
             lista_homen_acima.append(altura)
         elif altura<1.73:
             lista_homen_abaixo.append(altura)
-          
-print(DA)
+quantidade_h_ac=lista_homen_acima.count()
+quantidade_h_ab=lista_homen_abaixo.count()
+quantidade_m_ac=lista_mulher_acima.count()
+quantidade_m_ab= lista_mulher_abaixo.count()
+print(f'A {quantidade_m_ac} de mulheres acima da média de altura e {quantidade_m_ab} abaixo dessa média.')
+print(f'A {quantidade_h_ac} de homens acima da média de altura e {quantidade_h_ab} abaixo dessa média.')
 
